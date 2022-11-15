20f74c4c2d01b86b1e1bb7a676dad29a09780a1d
GrimMori <armigofire@gmail.com>
2022-11-15 17:38:40 +0100

    slideshow working

--- python-pill.py
+++ python-pill.py
@@ -7,7 +7,9 @@ from pygame.locals import *
 pygame.init()
 
 fpsClock = pygame.time.Clock()
-window = (800, 600) # size of the window
+window_width = 800
+window_height = 600
+window = (window_width, window_height) # size of the window
 surface = pygame.display.set_mode(window) #opens up a window
 icon = pygame.image.load('../yippee.jpeg') # loads an image
 pygame.display.set_icon(icon) # uses image as icon for the window
@@ -20,8 +22,9 @@ files = [] # new list that will contain the slides
 # but we want each element of the list to be added to each new index of 'files'.
 files.extend(os.listdir(folder))
 files.sort() # sorts the list in ascending order (alphabetical by default)
+x = 0
 
-
+# load images
 
 while True:
     for event in pygame.event.get(): # handle inputs
@@ -29,7 +32,27 @@ while True:
             pygame.quit()
             sys.exit() # close window
         if event.type == MOUSEBUTTONDOWN: # when mouse is clicked
-            print(files)
-        
+            if x < len(files)-1:
+                x += 1
+                print(files[x])
+                path = folder + files[x]
+                print(str(path))
+                img = pygame.image.load(path)
+            else:
+                print("end!")
+                
+            surface.blit(img, (0, 0))
     pygame.display.update()
     fpsClock.tick(30)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
